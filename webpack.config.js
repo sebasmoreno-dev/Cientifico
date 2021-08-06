9aaab9834c978d462cd96107c927e9dca56a9fe5
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2021-08-06 17:42:28 -0500

    Bug fixed, solucionando un bug del loader de sass

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main .js'
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    mode: 'productio',
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                inject: 'true',
+                template: './public/index.html',
+                filename: './index.html'
+            }
+        ),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+
+        new CopyWebpackPlugin({
+            patterns:[{
+                from: './src/styles/styles.css',
+                to: ''
+            }],
+        }),
+        new CopyWebpackPlugin({
+            patterns:[{
+                from: './src/images',
+                to: './src/images/'
+            }]
+        })
+    ]
+}
